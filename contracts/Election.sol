886eefb5dbaff42a3f9b25fa6c23a9811141902f
sonish <sonishmzn95@gmail.com>
2019-04-30 20:35:23 +0545

    First step to blockchain

---
+++ contracts/Election.sol
@@ -0,0 +1,18 @@
+pragma solidity >=0.4.22 <0.6.0;
+
+contract Election{
+	//Store candidate
+	// Read Candidate
+	//Constructor
+
+	//when you assign public solidty directly generates getter function
+	string public candidate;
+
+	constructor() public{
+		candidate = "Candidate1";
+
+
+	}
+
+
+}
